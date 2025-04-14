7eae3bd62b5f0e87a24f1bde52445f7ad9accbae
Santiago Alarcón <santi14605@gmail.com>
2025-04-14 16:45:53 -0300

    Add files via upload

---
+++ ArbolBinario.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace tp2
+{
+	public class ArbolBinario<T>
+	{
+		
+		private T dato;
+		private ArbolBinario<T> hijoIzquierdo;
+		private ArbolBinario<T> hijoDerecho;
+	
+		
+		public ArbolBinario(T dato) {
+			this.dato = dato;
+		}
+	
+		public T getDatoRaiz() {
+			return this.dato;
+		}
+	
+		public ArbolBinario<T> getHijoIzquierdo() {
+			return this.hijoIzquierdo;
+		}
+	
+		public ArbolBinario<T> getHijoDerecho() {
+			return this.hijoDerecho;
+		}
+	
+		public void agregarHijoIzquierdo(ArbolBinario<T> hijo) {
+			this.hijoIzquierdo=hijo;
+		}
+	
+		public void agregarHijoDerecho(ArbolBinario<T> hijo) {
+			this.hijoDerecho=hijo;
+		}
+	
+		public void eliminarHijoIzquierdo() {
+			this.hijoIzquierdo=null;
+		}
+	
+		public void eliminarHijoDerecho() {
+			this.hijoDerecho=null;
+		}
+	
+		public bool esHoja() {
+			return this.hijoIzquierdo==null && this.hijoDerecho==null;
+		}
+		
+		public void inorden() {
+			if(this.hijoIzquierdo != null){
+				this.hijoIzquierdo.inorden();
+			}
+			
+			Console.WriteLine(this.dato + " ");
+			
+			if(this.hijoDerecho != null){
+				this.hijoDerecho.inorden();
+			}
+		}
+		
+		public void preorden() {
+		}
+		
+		public void postorden() {
+		}
+		
+		public void recorridoPorNiveles() {
+		}
+	
+		public int contarHojas() {
+			return 0;
+		}
+		
+		public void recorridoEntreNiveles(int n,int m) {
+		}
+	}
+}
